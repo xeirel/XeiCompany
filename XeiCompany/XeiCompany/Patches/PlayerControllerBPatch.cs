181e84f6159e0218093bbd493fddc66b784efc83
Xeirel <altay.ertekin@gmail.com>
2024-02-23 22:10:38 +0300

    Project Created

---
+++ XeiCompany/XeiCompany/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,32 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace XeiCompany.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix] 
+        static void carryWeightPatch(ref float ___carryWeight)
+        {
+            ___carryWeight = 1f;
+        }
+
+    }
+    internal class PlayerControllerGUIPatch
+    {
+        [HarmonyPatch("OnGUI")]
+        [HarmonyPostfix]
+        static void OnGUIPatch()
+        {
+            Rect labelRect = new Rect(10, 10, 200, 20);
+            string labelText = "XeiCompany Loaded.";
+            GUIStyle style = GUI.skin.label;
+
+            GUI.Label(labelRect, labelText, style);
+        }
+    }
+}
---
+++ XeiCompany/XeiCompany/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using XeiCompany.Patches;
+
+
+namespace XeiCompany
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class XeiCompanyBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Xeirel.XeiCompany";
+        private const string modName = "Xei Company";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static XeiCompanyBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null) { Instance = this; }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("XeiCompany Has Awaken...");
+            mls.LogInfo("XeiCompany Has Awaken...");
+            mls.LogInfo("XeiCompany Has Awaken...");
+            mls.LogInfo("XeiCompany Has Awaken...");
+            mls.LogInfo("XeiCompany Has Awaken...");
+            mls.LogInfo("XeiCompany Has Awaken...");
+
+            harmony.PatchAll(typeof(XeiCompanyBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+            harmony.PatchAll(typeof(PlayerControllerGUIPatch));
+        }
+    }
+}
